1ade7695989034a45c9e431a3afcfd0722f261af
StephNdungu <stephy.wanjiku@gmail.com>
2024-07-21 23:53:15 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  
+  const square = (x) => {
+    return x * x;
+  };
+  
+  
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
